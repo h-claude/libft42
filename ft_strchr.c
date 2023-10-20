f659884c4cdb020f491a3fd9ef41eb55fb154c70
Hippolyte Claude <hclaude@k2r2p1.42mulhouse.fr>
2023-10-20 15:46:40 +0200

    MAJ

--- ft_strchr.c
+++ ft_strchr.c
@@ -6,7 +6,43 @@
 /*   By: hclaude <hclaude@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/10/20 14:27:02 by hclaude           #+#    #+#             */
-/*   Updated: 2023/10/20 14:27:03 by hclaude          ###   ########.fr       */
+/*   Updated: 2023/10/20 15:24:50 by hclaude          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include <string.h>
+
+char *ft_strchr(const char *s, int c)
+{
+	int 	i;
+	char 	*result;
+
+	i = 0;
+	while (s[i] != '\0')
+	{
+		if (s[i] == c)
+		{
+			*result = s[i];
+			return (result);
+		}
+		i++;
+	}
+	if (s[i] == c)
+	{
+		return (result);
+	}
+	return (NULL);
+}
+
+#include <stdio.h>
+
+int main(void)
+{
+	char test[] = "test";
+	int c = 't';
+	char test1[] = "test";
+	int c1 = 't';
+	
+	printf("%s\n", ft_strchr(test, c));
+	printf("%s", strchr(test1, c1));
+}
