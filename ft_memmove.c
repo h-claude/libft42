c1c3d81adf6731a976df038de5e31c3639c6a594
Hippolyte Claude <hclaude@k2r1p9.42mulhouse.fr>
2023-10-27 17:27:46 +0200

    memmove

--- ft_memmove.c
+++ ft_memmove.c
@@ -6,47 +6,52 @@
 /*   By: hclaude <hclaude@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/10/18 13:02:22 by hclaude           #+#    #+#             */
-/*   Updated: 2023/10/27 15:40:37 by hclaude          ###   ########.fr       */
+/*   Updated: 2023/10/27 17:21:47 by hclaude          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-void	*ft_memmove(void *dst, const void *src, t_size_t len)
+void	*ft_memmove(void *dest, const void *src, t_size_t n)
 {
-	char		*dest;
+	char		*dst;
 	const char	*source;
 	t_size_t	i;
 
-	dest = dst;
+	dst = dest;
 	source = src;
-	i = len;
-	if (source == dest)
-		return (dst);
-	if (dest < source || dest >= source + i)
+	i = 0;
+	if (dest < src)
 	{
-		while (i--)
-			dest[i - 1] = source[i - 1];
+		while (i < n)
+		{
+			dst[i] = source[i];
+			i++;
+		}
 	}
 	else
 	{
-		while (i--)
-			dest[len - i] = source[len - i];
+		while (n != 0)
+		{
+			dst[n - 1] = source[n - 1];
+			n--;
+		}
 	}
-	return (dst);
+	return (dest);
 }
 
 // #include <stdio.h>
+// #include <string.h>
 
 // int main(void)
 // {
 // 	char dest2[] = "Hello World";
-// 	char src2[] = "Goodbye World";
+// 	//char src2[] = "Goodbye World";
 // 	char dest3[] = "Hello World";
-// 	char src3[] = "Goodbye World";
+// 	//char src3[] = "Goodbye World";
 
-// 	ft_memmove(dest2, src2, 5);
-// 	memmove(dest3, src3, 5);
+// 	ft_memmove(dest2, dest2, 1);
+// 	memmove(dest3, dest3, 1);
 // 	printf("ft_memmove : %s\n", dest2);
 // 	printf("memmove : %s\n", dest3);
 // }
