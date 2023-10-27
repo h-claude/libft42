ec9c97538dfb18f5f3c32ab2e0db5a33966275e8
Hippolyte Claude <hclaude@k2r1p9.42mulhouse.fr>
2023-10-27 17:28:15 +0200

    memmove

--- ft_memmove.c
+++ ft_memmove.c
@@ -6,7 +6,7 @@
 /*   By: hclaude <hclaude@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/10/18 13:02:22 by hclaude           #+#    #+#             */
-/*   Updated: 2023/10/27 17:21:47 by hclaude          ###   ########.fr       */
+/*   Updated: 2023/10/27 17:28:07 by hclaude          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -39,19 +39,3 @@ void	*ft_memmove(void *dest, const void *src, t_size_t n)
 	}
 	return (dest);
 }
-
-// #include <stdio.h>
-// #include <string.h>
-
-// int main(void)
-// {
-// 	char dest2[] = "Hello World";
-// 	//char src2[] = "Goodbye World";
-// 	char dest3[] = "Hello World";
-// 	//char src3[] = "Goodbye World";
-
-// 	ft_memmove(dest2, dest2, 1);
-// 	memmove(dest3, dest3, 1);
-// 	printf("ft_memmove : %s\n", dest2);
-// 	printf("memmove : %s\n", dest3);
-// }
