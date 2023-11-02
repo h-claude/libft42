bd95029bcf542941a52146a7f7289c6e5ec37081
Hippolyte Claude <hclaude@k2r1p2.42mulhouse.fr>
2023-11-02 16:39:53 +0100

    split

--- ft_split.c
+++ ft_split.c
@@ -3,50 +3,102 @@
 /*                                                        :::      ::::::::   */
 /*   ft_split.c                                         :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: hclaude <hclaude@student.42mulhouse.fr>    +#+  +:+       +#+        */
+/*   By: hclaude <hclaude@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/11/01 15:49:25 by hclaude           #+#    #+#             */
-/*   Updated: 2023/11/02 04:21:40 by hclaude          ###   ########.fr       */
+/*   Updated: 2023/11/02 16:39:00 by hclaude          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 #include <stdlib.h>
 
-static char	**freetab(char **s)
+static int    countstr(char const *s, char c)
 {
-	size_t	i;
+    int    i;
+    int    nbstr;
+
+    i = 0;
+    nbstr = 0;
+    while (s[i])
+    {
+        while (s[i] == c)
+            i++;
+        if (s[i] != c && s[i] != '\0')
+            nbstr++;
+        while (s[i] != c && s[i] != '\0')
+            i++;
+    }
+    return (nbstr);
+}
+
+static void	freetab(char **str, int len)
+{
+	int i;
 
 	i = 0;
-	while (s[i])
+	while (i <= len)
 	{
-		free(s[i]);
+		free(str[i]);
 		i++;
 	}
-	free(s[i]);
-	return (s);
 }
 
-static int	countstr(char const *s, char c)
+static char    **sub(const char *s, char c, char **str)
 {
-	int	i;
-	int	nbstr;
+    size_t    	i;
+    size_t    	itemp;
+    int        	y;
+    int 		len;
+
+    i = 0;
+    itemp = 0;
+    y = 0;
+    len = countstr(s, c);
+    while (y < len)
+    {
+        while (s[i] == c && s[i])
+            i++;
+        itemp = i;
+        while (s[i] != c && s[i])
+            i++;
+        str[y] = ft_substr(s, itemp, i - itemp);
+        if (!str[y])
+            freetab(str, len);
+			return (NULL);
+        y++;
+    }
+    str[y] = NULL;
+    return (str);
+}
+
+char    **ft_split(char const *s, char c)
+{
+    char    **str;
+
+    if (!s)
+        return (NULL);
+    str = (char **)ft_calloc(countstr(s, c) + 1, sizeof(char *));;
+    if (!str)
+        return (NULL);
+    if (sub(s, c, str))
+        return (NULL);
+    return (str);
+}
+
+int main(void)
+{
+	int i;
 
 	i = 0;
-	nbstr = 0;
-	while (s[i])
+	char **tab = ft_split("Hello World", ' ');
+	while (tab[i] != NULL)
 	{
-		while (s[i] == c)
-			i++;
-		if (s[i] != c && s[i] != '\0')
-			nbstr++;
-		while (s[i] != c && s[i] != '\0')
-			i++;
+		printf("%s", tab[i]);
+		i++;
 	}
-	return (nbstr);
-}
-
-char	**ft_split(char const *s, char c)
-{
-	
+	// for (int i = 0; tab[i]; i++)
+	// {
+	// 	printf("%s\n", tab[i]);
+	// }
 }
